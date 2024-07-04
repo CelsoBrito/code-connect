ea6bc2c2311f835c2e9d66845bb3964e393fc373
Celso Brito <celsofilho_ap@hotmail.com>
2024-07-03 22:32:27 -0300

    :handle: Change the tag of Image inside the aside Component

--- src/app/components/Aside/index.tsx
+++ src/app/components/Aside/index.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image';
+import logo from './logo.png';
+
 export const Aside = () => {
     return (
         <aside className="rounded-[8px] bg-[#171D1F] py-[40px] px-[16px]">
-            <img src="/logo.png" alt="Logo da Code Connect" />
+            <Image src={logo} alt='Logo da Code Connect' />
+            {/* <img src="/logo.png" alt="Logo da Code Connect" /> */}
         </aside>
     );
 }
